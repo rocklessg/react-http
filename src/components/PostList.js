372592884b12dbce5a0624652ed9f6958fb7f0f1
rocklessg <abdulhafiz.suleiman@decagon.dev>
2023-02-22 06:44:53 +0100

    HTTP Get request

---
+++ src/components/PostList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class PostList extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         posts: [],
+         error: ''
+      }
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/post')
+        .then(response => {
+            console.log(response)
+            this.setState({
+                posts: response.data
+            })
+        })
+        .catch(error => {
+            console.log(error)
+            this.setState({error: 'Something went wrong. Error retrieving data'})
+        })
+    }
+
+  render() {
+    const { posts, error } = this.state
+    return (
+      <div>
+        List of posts
+        {
+            posts.length ?
+            posts.map(post => <div key={post.id}>{post.title}</div>) :
+            null
+        }
+        {
+            error ? <div>{error}</div> : null
+        }
+      </div>
+    )
+  }
+}
+
+export default PostList
