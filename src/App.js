60b801ba4a87c139295d0b03c111dd31d643acf7
rocklessg <abdulhafiz.suleiman@decagon.dev>
2023-03-07 07:21:39 +0100

    Use Effect Hook

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import PostForm from './components/PostForm';
 import HookCounter from './components/HookCounter';
 import HookCounter2 from './components/HookCounter2';
 import HookCounter3 from './components/HookCounter3';
+import EffectCount from './components/EffectCount';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter3 />
+      < EffectCount />
+      {/* <HookCounter3 /> */}
       {/* < HookCounter2 /> */}
       {/* < HookCounter /> */}
       {/* < PostForm /> */}
